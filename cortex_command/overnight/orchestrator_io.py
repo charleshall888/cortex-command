7a239d88555688477f3d05d9b623fcb1421ad8c6
charleshall888 <charlie.hall@cfacorp.com>
2026-04-29 08:18:45 -0400

    Re-export aggregate_round_context from orchestrator_io sanctioned surface

--- cortex_command/overnight/orchestrator_io.py
+++ cortex_command/overnight/orchestrator_io.py
@@ -7,9 +7,11 @@ modules.  It contains no new logic.
 """
 
 from cortex_command.overnight.deferral import write_escalation
+from cortex_command.overnight.orchestrator_context import aggregate_round_context
 from cortex_command.overnight.state import load_state, save_state, update_feature_status
 
 __all__ = [
+    "aggregate_round_context",
     "load_state",
     "save_state",
     "update_feature_status",
